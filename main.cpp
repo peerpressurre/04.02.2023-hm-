8247357598ae910b2e705a826e7a8bb83bc859fd
peerpressurre <gsophiay@gmail.com>
2023-02-15 13:57:04 +0200

    6ff

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+bool TrueFalse(T n)
+{
+	if (n > 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+int main()
+{
+	int n;
+	cout << "Enter num: ";
+	cin >> n;
+
+	cout << TrueFalse(n);
+	if (TrueFalse(n) == 0)
+	{
+		cout << " (False)" << endl;
+	}
+	else if (TrueFalse(n) == 1)
+	{
+		cout << " (True)" << endl;
+	}
+	return 0;
+}
