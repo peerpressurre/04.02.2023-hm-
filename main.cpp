19930fae6439db5063a6d64afdf1aade2b8d2394
peerpressurre <113593875+peerpressurre@users.noreply.github.com>
2023-02-16 16:35:23 +0200

    2 2 f

--- main.cpp
+++ main.cpp
@@ -14,12 +14,15 @@ T MinMax(T arr[], T arr2[], const T size)
 	}
 	cout << endl;
 	cout << "Reversed Array:\t";
-	for (size_t i = size -1, j = 0; i >= 0; i--, j++)
+	for (size_t i = size - 1, j = 0; i >= 0; i--, j++)
 	{
 		arr2[j] = arr[i];
 		cout << arr2[j] << "\t";
+		if (i == 0)
+		{
+			break;
+		}
 	}
-
 	return 0;
 }
 
@@ -29,6 +32,5 @@ int main()
 	int arr[size];
 	int arr2[size];
 	MinMax(arr, arr2, size);
-
 	return 0;
 }
