df5ffd15886cb31fdab98e1f46f4d5dcc4caa32e
peerpressurre <113593875+peerpressurre@users.noreply.github.com>
2023-02-16 17:10:52 +0200

    2 3f

--- main.cpp
+++ main.cpp
@@ -4,25 +4,33 @@
 using namespace std;
 
 template<typename T>
-T MinMax(T arr[], T arr2[], const T size)
-{
-	cout << "Original Array:\t";
+T MinMax(T arr[], const T size)
+{   
+        int primes = 0;
+	cout << "Array:\t";
 	for (size_t i = 0; i < size; i++)
 	{
-		arr[i] = rand() % 10;
+		arr[i] = rand() % 20;
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
-	cout << "Reversed Array:\t";
-	for (size_t i = size - 1, j = 0; i >= 0; i--, j++)
+
+	for (size_t i = 0; i < size; i++)
 	{
-		arr2[j] = arr[i];
-		cout << arr2[j] << "\t";
-		if (i == 0)
+		int counter = 0;
+		for (size_t j = 1; j < 20; j++)
 		{
-			break;
+			if (arr[i] % j == 0)
+			{
+				counter++;
+			}
+		}
+		if (counter == 2)
+		{
+			primes++;
 		}
 	}
+	cout << "Number of prime numbers: " << primes << endl;
 	return 0;
 }
 
@@ -30,7 +38,6 @@ int main()
 {
 	const int size = 10;
 	int arr[size];
-	int arr2[size];
-	MinMax(arr, arr2, size);
+	MinMax(arr, size);
 	return 0;
 }
